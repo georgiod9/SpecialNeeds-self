4fd5c4927bba20dafb202c92760ecf17b677602c
Georgio <georgiod9@gmail.com>
2020-12-14 19:01:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/specialneeds/DatabaseAccess.java
@@ -0,0 +1,53 @@
+package com.example.specialneeds;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseAccess {
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase database = null;
+    private static DatabaseAccess instance;
+
+    private DatabaseAccess(Context context){
+        this.openHelper = new DatabaseOpenHelper(context);
+    }
+
+    public static DatabaseAccess getInstance(Context context){
+        if (instance == null){
+            instance = new DatabaseAccess(context);
+        }
+        return instance;
+    }
+
+    public void open(){
+        this.database = openHelper.getWritableDatabase();
+    }
+
+    public void close(){
+        if (database != null){
+            this.database.close();
+        }
+    }
+
+    public List<String> getData(){
+        List<String> list = new ArrayList<>();
+        Cursor cursor = database.rawQuery("SELECT * FROM User", null);
+        cursor.moveToNext();
+
+        while(!cursor.isAfterLast()){
+            list.add(cursor.getString(2));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return list;
+    }
+
+    public void addData(){
+        
+    }
+}
